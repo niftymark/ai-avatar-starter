9ceda46518f9d94dbd11b4cb6f6099d71462d2c6
niftymark <mark.j.engelhardt@gmail.com>
2023-01-11 12:42:55 +0100

    firstWorkingVersion v.1.0

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="AI Avatar Generator" key="title" />
-        <meta
-          property="og:description"
-          content="build with buildspace"
-          key="description"
-        />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="og:title" content="Avatar Generator" key="title" />
+        <meta property="og:description" content="build" key="description" />
+        {/* <meta name="twitter:card" content="summary_large_image"></meta> */}
       </Head>
       <body>
         <Main />
